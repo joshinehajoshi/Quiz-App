e79bc7af8fd02a6e1204f0a7d168093dcf680a88
Neha Joshi <nehajoshigkp@gmail.com>
2022-01-06 20:52:30 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import React from 'react';
+import {
+BrowserRouter as Router,
+Switch,
+Route
+} from 'react-router-dom';
+import Setting from './Pages/Setting';
+import Questions from './Pages/Questions';
+import FinalScreen from './Pages/FinalScreen';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+  <Router>
+    <Switch>
+      <Route path='/' exact>
+        <Setting />
+      </Route>
+      <Route path='/questions'>
+        <Questions />
+      </Route>
+      <Route path='/finalScreen'>
+        <FinalScreen />
+      </Route>
+    </Switch>
+  </Router>
   );
 }
 
