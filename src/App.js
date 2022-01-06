e0a5b03d24412cbb4b1fefc679caa8a6d553d04f
Neha Joshi <nehajoshigkp@gmail.com>
2022-01-06 16:19:29 +0530

    files deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import QuestionBox from './components/QuestionBox';
-import Result from './components/Result';
 
 function App() {
   return (
     <div className="App">
-      <QuestionBox />
-      <Result />
+      
     </div>
   );
 }
